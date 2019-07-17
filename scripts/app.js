55b5b7101d57f4050dde963f4544175c8ba1380a
Junaid Farooq <junaid@evercam.io>
2019-07-17 11:07:08 +0500

    Start showing icon and day night images.

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,13 @@ const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
 
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+
 const updateUI = (data) => {
 
   const { cityDets, weather } = data;
-
+  console.log(weather);
   details.innerHTML = `
     <h5 class="my-3">${cityDets.EnglishName}</h5>
     <div class="my-3">${weather.WeatherText}</div>
@@ -14,6 +17,13 @@ const updateUI = (data) => {
       <span>&deg;</span>
     </div>
   `
+
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`
+  icon.setAttribute("src", iconSrc)
+
+  let timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
+
+  time.setAttribute("src", timeSrc);
 
   if (card.classList.contains("d-none")) {
     card.classList.remove("d-none")
